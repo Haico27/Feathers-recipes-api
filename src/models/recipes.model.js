34d5e972186dba80a30ac2a7c8267be462255476
Haico27 <haico.wensink@gmail.com>
2017-05-15 17:14:29 +0700

    adjusted Schema recipe-model

--- src/models/recipes.model.js
+++ src/models/recipes.model.js
@@ -5,11 +5,43 @@
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
-  const recipes = new mongooseClient.Schema({
-    text: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
+
+  const Schema = mongooseClient.Schema;
+
+  const ingredientSchema = new Schema({
+    amount: { type: String, required: false },
+    name: { type: String, required: true },
+    optional: { type: Boolean, required: true, default: false }
   });
 
-  return mongooseClient.model('recipes', recipes);
+  const cookingStepSchema = new Schema({
+    cookingTime: { type: Number, required: false }, // in minutes
+    title: { type: String, required: false },
+    description: { type: String, required: true }
+  });
+
+  const userSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: 'user' },
+    name: { type: String, required: true }
+  });
+
+  const recipeSchema = new Schema({
+    title: { type: String, required: true },
+    summary: { type: String, required: true },
+    photo: { type: String, required: true },
+    vegan: { type: Boolean, required: true, 'default': false },
+    vegetarian: { type: Boolean, required: true, 'default': false },
+    pescatarian: { type: Boolean, required: true, 'default': false },
+    cookingTime: { type: Number, required: false }, // in minutes
+    ingredients: [ingredientSchema],
+    cookingSteps: [cookingStepSchema],
+    likedBy: [ userSchema ],
+    authorId: { type: Schema.Types.ObjectId, ref: 'user' },
+    createdAt: { type: Date, 'default': Date.now },
+    updatedAt: { type: Date, 'default': Date.now }
+  });
+
+
+
+  return mongooseClient.model('recipes', recipeSchema);
 };
