82b3f187b71b63dc939c3a33774e10616e25b5d6
Haico27 <haico.wensink@gmail.com>
2017-05-17 19:02:52 +0700

    added hook for recipes service for assigning author to the recipes

--- src/services/recipes/recipes.hooks.js
+++ src/services/recipes/recipes.hooks.js
@@ -1,18 +1,39 @@
+'use strict';
+
 const { authenticate } = require('feathers-authentication').hooks;
+const common = require('feathers-hooks-common');
+
+//before hook: assign authorId to the _id of the currently logged in user.
+const assignAuthor = function(options) {
+  return function(hook) {
+    //The authenticated user
+    const user = hook.params.user;
+    hook.data.authorId = user._id;
+  }
+}
+
+//after hook: look up the user with the matching authorId in the users service
+//and add is as 'author'
+const populateAuthor = common.populate('author', { service: 'users', field: 'authorId' })
 
 module.exports = {
   before: {
     all: [  ],
     find: [],
     get: [],
-    create: [ authenticate('jwt') ],
+    create: [
+      authenticate('jwt'),
+      assignAuthor()
+    ],
     update: [ authenticate('jwt') ],
     patch: [ authenticate('jwt') ],
     remove: [ authenticate('jwt') ]
   },
 
   after: {
-    all: [],
+    all: [
+      populateAuthor
+    ],
     find: [],
     get: [],
     create: [],
